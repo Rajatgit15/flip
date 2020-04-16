68bd78dd5192aad025363771b2f7eaa19184dd15
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-16 15:45:29 +0530

    Updates

---
+++ precache-manifest.87e6484c1e748b5ab68dbaec85d5b5fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d864c1284cd4a2285a62ade7d0f1ec7e",
+    "url": "/flip/index.html"
+  },
+  {
+    "revision": "d1bb5b52c0f85a0b3bac",
+    "url": "/flip/static/css/main.fb0316b9.chunk.css"
+  },
+  {
+    "revision": "7f22acf8ea8a1a37b289",
+    "url": "/flip/static/js/2.6f41c62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/flip/static/js/2.6f41c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bb5b52c0f85a0b3bac",
+    "url": "/flip/static/js/main.10f14060.chunk.js"
+  },
+  {
+    "revision": "d4b5ddadffe0866a766e",
+    "url": "/flip/static/js/runtime-main.f23e335d.js"
+  }
+]);
